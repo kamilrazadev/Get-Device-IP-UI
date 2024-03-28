6e247b9e484effcaa7880507b8b95d73bf527ffc
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-03-28 21:45:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [deviceIp, setDeviceIp] = useState("");
+  const backendUrl = "http://localhost:3000";
+
+  const getDeviceIP = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/get-ip`);
+      setDeviceIp(response.data.ip);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div style={{ marginBottom: "10px" }}>Device IP: {deviceIp}</div>
+      <button onClick={getDeviceIP}>Send Request</button>
+    </>
+  );
+}
+
+export default App;
